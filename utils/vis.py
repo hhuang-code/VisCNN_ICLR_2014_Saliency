e3441af8aeb3c6f418b75ea2b6fd7471f3e439a2
huanghao-code <huanghao_bupt@sina.com>
2018-02-19 22:29:14 -0500

    display images

--- utils/vis.py
+++ utils/vis.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 import pdb
+
+def postprocess(img):
+    img = (img - img.min()) / (img.max() - img.min()) * 255
+    img = img.astype(np.uint8)
+
+    return img
 
 """
 generate and display saliency maps
@@ -12,10 +19,10 @@ def show_saliency_maps(images, saliency_maps):
 
     for i in range(N):
         plt.subplot(2, N, i + 1)
-        plt.imshow(images[i].data.numpy().transpose(1, 2, 0))
+        plt.imshow(postprocess(images[i].data.numpy().transpose(1, 2, 0)))
         plt.axis('off')
         plt.subplot(2, N, N + i + 1)
-        plt.imshow(saliency_maps[i].data.numpy(), cmap = plt.cm.hot)
+        plt.imshow(postprocess(saliency_maps[i].numpy()), cmap = plt.cm.hot)
         plt.axis('off')
         plt.gcf().set_size_inches(12, 5)
     plt.show()
